8c18b9a2ac4fe666c4dc68e8e239ea8896413903
Mauro <johernandezlope76z@gmail.com>
2022-10-26 11:27:15 -0500

    eth

--- src/componentes/FormDivisa.js
+++ src/componentes/FormDivisa.js
@@ -22,6 +22,9 @@ const FormDivisa = () => {
     let x = parseFloat(cant);
     let x2 = 0;
     if (divun === "MXN") {
+      if (divdo === "MXN") {
+        x2 = 1;
+      }
       if (divdo === "USD") {
         x2 = 0.05;
       } else if (divdo === "EUR") {
@@ -32,6 +35,9 @@ const FormDivisa = () => {
         x2 = 0.85;
       }
     } else if (divun === "USD") {
+      if (divdo === "USD") {
+        x2 = 1;
+      }
       if (divdo === "MXN") {
         x2 = 19.93;
       } else if (divdo === "EUR") {
@@ -42,6 +48,9 @@ const FormDivisa = () => {
         x2 = 16.92;
       }
     } else if (divun === "EUR") {
+      if (divdo === "EUR") {
+        x2 = 1;
+      }
       if (divdo === "USD") {
         x2 = 0.99;
       } else if (divdo === "MXN") {
@@ -52,6 +61,9 @@ const FormDivisa = () => {
         x2 = 16.7;
       }
     } else if (divun === "BTC") {
+      if (divdo === "BTC") {
+        x2 = 1;
+      }
       if (divdo === "USD") {
         x2 = 19329.8;
       } else if (divdo === "EUR") {
@@ -62,15 +74,34 @@ const FormDivisa = () => {
         x2 = 327142.37;
       }
     } else if (divun === "DOGE") {
+      if (divdo === "DOGE") {
+        x2 = 1;
+      }
       if (divdo === "USD") {
         x2 = 0.059;
       } else if (divdo === "EUR") {
         x2 = 0.06;
       } else if (divdo === "BTC") {
         x2 = 0.0000031;
-      } else if (divdo === "mxn") {
+      } else if (divdo === "MXN") {
         x2 = 1.18;
       }
+    } else if (divun === "ETH") {
+      if (divdo === "ETH") {
+        x2 = 1;
+      }
+      if (divdo === "USD") {
+        x2 = 1564.3;
+      }else if (divdo === "EUR") {
+        x2 = 1552.4;
+      }else if (divdo === "EUR") {
+        x2 = 23.1;
+      } else if (divdo === "BTC") {
+        x2 = 0.074920;
+      } else if (divdo === "MXN") {
+        x2 = 31.06;
+      }
+      
     }
     let x3 = x * x2;
     setRes(x3);
@@ -82,9 +113,9 @@ const FormDivisa = () => {
   return (
     <form action="" onSubmit={onSubmit}>
       <p>Cantidad total: {resultado}</p>
-      <p>div 1: {cant}</p>
-      <p>div 1: {divun}</p>
-      <p>div 2: {divdo}</p>
+      <p>Cantidad a convertir: {cant}</p>
+      <p>Divisa por convertir: {divun}</p>
+      <p>Divisa a convertrir {divdo}</p>
 
       <div>
         <label htmlFor="cant">Cantidad a cambiar</label>
@@ -114,6 +145,7 @@ const FormDivisa = () => {
           id="divdo"
           name={"divdo"}
           value={divdo}
+          defaultValue={divdo}
           onChange={onChange}
         >
           <option value="MXN">MXN</option>
